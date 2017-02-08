cdeefd4e8ebe8f9be05925e8b9b483baf0f9839e
D Inbound <D_Inbound@hotmail.com>
2017-02-08 10:27:34 +0100

    Added script for editing

---
+++ Assets/Scripts/Editor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Editor : MonoBehaviour
+{
+    private Camera _camera;
+    
+    void Start()
+    {
+        foreach(Transform child in this.transform)
+        {
+            if (child.CompareTag("MainCamera"))
+                _camera = child.GetComponent<Camera>();
+        }
+    }
+
+    void Update()
+    {
+        Vector3 MousePos = Input.mousePosition;
+        Vector3 WorldPos = _camera.ScreenToWorldPoint(MousePos);
+        Debug.Log(WorldPos);
+    }
+}
